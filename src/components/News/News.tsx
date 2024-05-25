e512202a4c65c54d56d0c78e9580c006cbfb1b66
Vladimir Frolov <frolov_vo@inbox.ru>
2024-05-26 02:41:43 +0300

    feat: create news section

--- src/components/News/News.tsx
+++ src/components/News/News.tsx
@@ -2,14 +2,23 @@ import { useAppSelector } from 'src/service/hooks'
 import styles from './News.module.scss'
 import NewsItem from './NewsItem/NewsItem'
 import { getNews } from 'src/service/slices/newsSlice'
+import ItemSlider from '../ItemSlider/ItemSlider'
+import { Link } from 'react-router-dom'
 
 const News = () => {
   const newsArray = useAppSelector(getNews)
-  console.log(newsArray)
 
   return (
-    <section className={styles.news}>
+    <section className={styles.news} id="news">
       <h2 className={styles.title}>Новости</h2>
+      <ItemSlider>
+        {newsArray.map(item => (
+          <NewsItem data={item} key={item.id} />
+        ))}
+      </ItemSlider>
+      <Link to="#" className={styles.button}>
+        Все новости
+      </Link>
     </section>
   )
 }

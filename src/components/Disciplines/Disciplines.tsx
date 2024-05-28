27827892cfd3ece4eb50a9d2d82551aedaf8b347
Vladimir Frolov <frolov_vo@inbox.ru>
2024-05-28 03:11:06 +0300

    feat: add id

--- src/components/Disciplines/Disciplines.tsx
+++ src/components/Disciplines/Disciplines.tsx
@@ -4,7 +4,7 @@ import DisciplineCard from './DisciplineCard/DisciplineCard'
 
 const Disciplines = () => {
   return (
-    <section className={styles.disciplines}>
+    <section className={styles.disciplines} id="disciplines">
       <h2 className={styles.title}>Дисциплины</h2>
       <ul className={styles.list}>
         {disciplinesMockData.map(item => (
